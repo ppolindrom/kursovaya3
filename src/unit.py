7792af33f99f7b1769caa374df3034c867ddaaa8
ppolindrom <ipollyce@mail.ru>
2023-05-02 19:16:11 +0300

    приписали докстринги к первым трем функциям

--- src/unit.py
+++ src/unit.py
@@ -2,11 +2,17 @@ import json
 
 
 def load_data():
+    """
+    выгружает json файл
+    """
     with open('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
 
 def get_last_op(operations):
+    """
+    выбирает из словаря 5 операций executed
+    """
     last_operation = []
     for i in operations[::-1]:
         if i['state'] == 'EXECUTED':
@@ -16,6 +22,9 @@ def get_last_op(operations):
     return last_operation
 
 def sorted_operation(operations):
+    """
+    сортирует по дате
+    """
     for i in range(len(operations) - 1):
         for j in range(len(operations) - i - 1):
             if operations[j]['date'] < operations[j + 1]['date']:
